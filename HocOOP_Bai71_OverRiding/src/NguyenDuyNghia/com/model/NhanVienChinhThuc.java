8058278960b159d3480c0f35efd75034171a0a46
ASUS <ASUS@192.168.1.3>
2020-02-23 16:22:28 +0700

    HocOOP_Bai71_OverRiding

---
+++ HocOOP_Bai71_OverRiding/src/NguyenDuyNghia/com/model/NhanVienChinhThuc.java
@@ -0,0 +1,16 @@
+package NguyenDuyNghia.com.model;
+
+public class NhanVienChinhThuc extends NhanVien {
+
+	@Override
+	public void tinhLuong(int ngayCong) {
+		// TODO Auto-generated method stub
+		if(ngayCong<20)
+			System.out.println("5 triệu");
+		else
+			System.out.println("20 triệu");
+
+		
+	}
+
+}
---
+++ HocOOP_Bai71_OverRiding/src/NguyenDuyNghia/com/test/testNhanVien.java
@@ -0,0 +1,15 @@
+package NguyenDuyNghia.com.test;
+import NguyenDuyNghia.com.model.*;
+public class testNhanVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NhanVienChinhThuc teo=new NhanVienChinhThuc();
+		teo.tinhLuong(25);
+		
+		NhanVienThoiVu ty=new NhanVienThoiVu();
+		ty.tinhLuong(3);
+
+	}
+
+}
